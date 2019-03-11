db65cf34acbab77ea26d35555a93751e44d1ca48
Marius Butnariu <marius.butnariu@yahoo.com>
2019-03-11 21:00:19 +0200

    cleanup js intro

--- intro.js
+++ intro.js
@@ -1,19 +1,3 @@
-console.info("my first js file")
-
-var name = "Marius";
-var employed = true;
-
-console.warn("employed");
-console.warn(employed);
-
-console.info("Name:");
-console.info(name);
-
-name = "Butnariu"
-
-console.info("Name:");
-console.info(name);
-
 function getWelcomeMsg() {
     return "Wellcome tu my site!";
 }
@@ -35,24 +19,8 @@ console.info("summaryElement", summaryElement);
 var color = getColor();
 summaryElement.style.color = color;
 
-// first example
-function hideHomePage() {
-    var el = document.getElementById('home-page');
-    el.style.display = 'none';
-}
-function showSkillsPage() {
-    document.getElementById('skills-page').style.display = 'block';
-}
-function clickOnSkills() {
-    hideHomePage();
-    showSkillsPage();
-    hidePage('home-page');
-    hidePage('languages-page');
-    hidePage('about-page');
-    showPage('skills-page');
-    
-}
-// first example -end 
+
+
 
 function hidePage(page) {
     var el = document.getElementById(page);
