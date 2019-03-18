2174f5aa08f699b67c519d77f6f4129373b6e730
Marius Butnariu <marius.butnariu@yahoo.com>
2019-03-18 19:56:16 +0200

    show skills with for

--- intro.js
+++ intro.js
@@ -24,8 +24,41 @@ summaryElement.style.color = color;
 
 function hidePage(page) {
     var el = document.getElementById(page);
-    el.style.display ='none';
-} 
+    el.style.display = 'none';
+}
 function showPage(page) {
     document.getElementById(page).style.display = 'block';
 }
+
+function initMenu() {
+    var links = document.querySelectorAll("#top-menu-bar a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = clickOnMenuItem;
+    }
+}
+
+function clickOnMenuItem () {
+     hideAllPages();
+     var pageId = this.getAttribute('data-page');
+     showPage(pageId);
+}
+
+function hideAllPages() {
+    var pages = document.querySelectorAll(".page");
+    for(var i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+}
+
+
+initMenu();
+
+function showSkills() {
+    var skills = ['html', 'css', 'js'];
+    console.warn('showSkills', skills);
+    for(var i = 0; i < skills.length; i++) {
+        console.info("#" + (i + 1) + " " + skills[i]);
+    }
+}
+
+showSkills();
